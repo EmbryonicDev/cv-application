543c63f383b291e3b5a31ff3299de72518794870
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-08-06 20:08:09 +0200

    Add basic styling to user details

--- src/Components/PersonalPage.js
+++ src/Components/PersonalPage.js
@@ -4,11 +4,11 @@ export default function PersonalPage(props) {
   return (
     <div id="personalPage">
       <h4 className="cvHeading">Name: </h4>
-      <h4>{` ${firstName} ${lastName}`}</h4>
+      <h4 className="userDetails" id='name'>{` ${firstName} ${lastName}`}</h4>
       <h4 className="cvHeading">Email: </h4>
-      <h4 id="email">{email}</h4>
+      <h4 className="userDetails" id="email">{email}</h4>
       <h4 className="cvHeading">Telephone Number: </h4>
-      <h4 id="tel"> {tel}</h4>
+      <h4 className="userDetails" id="tel"> {tel}</h4>
     </div>
   )
 }
