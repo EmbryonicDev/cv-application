e107fabd39e695ca635f6f3d1a27535fff7a48ed
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-08-06 21:24:15 +0200

    Render EducationForm to page

--- src/Components/EducationForm.js
+++ src/Components/EducationForm.js
@@ -1,7 +1,43 @@
-export default function EducationForm() {
+export default function EducationForm(props) {
+  const { submitEducation, handleChange, data } = props;
+
   return (
     <div className="educationForm">
-      Education Form goes here
+      <form action="#" onSubmit={submitEducation}>
+        <p className="personalInput">
+          <label htmlFor="schoolName">School / Institute Name:</label>
+          <input
+            type="text"
+            id="schoolName"
+            name="schoolName"
+            value={data.schoolName}
+            onChange={(event) => handleChange('education', event)}
+          />
+        </p>
+        <p className="personalInput">
+          <label htmlFor="titleOfStudy">Title of Study:</label>
+          <input
+            type="text"
+            id="titleOfStudy"
+            name="titleOfStudy"
+            value={data.titleOfStudy}
+            onChange={(event) => handleChange('education', event)}
+          />
+        </p>
+        <p className="personalInput">
+          <label htmlFor="studyDate">Completion Date:</label>
+          <input
+            type="date"
+            id="studyDate"
+            name="studyDate"
+            value={data.studyDate}
+            onChange={(event) => handleChange('education', event)}
+          />
+        </p>
+        <button id="personalSubmit" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   )
 }
