e092252d5593aab69e03e66662b22c5b9c03802e
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-08-07 13:04:37 +0200

    Create & code out EducationDiv.js

---
+++ src/Components/EducationDiv.js
@@ -0,0 +1,15 @@
+export default function EducationDiv(props) {
+  const { school, title, date, index } = props
+
+  return (
+    <div className="educationDiv">
+      <h1 categoryHeading>Education {index}</h1>
+      <h4 className="pageLabel">School / Institute Name: </h4>
+      <h4 className="pageData">{school}</h4>
+      <h4 className="pageLabel">Title of Study: </h4>
+      <h4 className="pageData">{title}</h4>
+      <h4 className="pageLabel">Date of Completion: </h4>
+      <h4 className="pageData"> {date}</h4>
+    </div>
+  )
+}
