62a6e993b6602904e327c0535f9adb47c67fec0e
djdevlam <embryonic.dev@gmail.com>
2022-08-08 15:22:51 +0200

    Populate ExperienceDiv.js

---
+++ src/Components/ExperienceDiv.js
@@ -0,0 +1,18 @@
+export default function ExperienceDiv(props) {
+  const { workName, workTitle, jobTasks, startDate, endDate, index } = props
+  return (
+    <div className="experienceDiv">
+      <h1 className='categoryHeading'>Work History {index}</h1>
+      <h4 className="pageLabel">Company Name: </h4>
+      <h4 className="pageData">{workName}</h4>
+      <h4 className="pageLabel">Job Title: </h4>
+      <h4 className="pageData">{workTitle}</h4>
+      <h4 className="pageLabel">Main Tasks of Job: </h4>
+      <h4 className="pageData">{jobTasks}</h4>
+      <h4 className="pageLabel">Start Date: </h4>
+      <h4 className="pageData"> {startDate}</h4>
+      <h4 className="pageLabel">End Date: </h4>
+      <h4 className="pageData"> {endDate}</h4>
+    </div>
+  )
+}
