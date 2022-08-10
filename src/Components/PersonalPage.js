21b798cb715701cb2bde2cc4ffb363bb0a84bd42
djdevlam <embryonic.dev@gmail.com>
2022-08-10 15:23:49 +0200

    Add new fields to PersonalPage

--- src/Components/PersonalPage.js
+++ src/Components/PersonalPage.js
@@ -1,5 +1,5 @@
 export default function PersonalPage(props) {
-  const { firstName, lastName, email, tel } = props.personalData;
+  const { firstName, lastName, email, tel, profession, address, website } = props.personalData;
   const { edit } = props;
 
   return (
@@ -7,10 +7,16 @@ export default function PersonalPage(props) {
       <h1 className='categoryHeading'>Personal Information</h1>
       <h4 className="pageLabel">Name: </h4>
       <h4 className="pageData" id='name'>{` ${firstName} ${lastName}`}</h4>
+      <h4 className="pageLabel">Profession: </h4>
+      <h4 className="pageData" id="profession">{profession}</h4>
       <h4 className="pageLabel">Email: </h4>
       <h4 className="pageData" id="email">{email}</h4>
       <h4 className="pageLabel">Telephone Number: </h4>
       <h4 className="pageData" id="tel"> {tel}</h4>
+      <h4 className="pageLabel">Address: </h4>
+      <h4 className="pageData" id="address">{address}</h4>
+      <h4 className="pageLabel">Website: </h4>
+      <h4 className="pageData" id="website"><a href={website}>{website}</a></h4>
       <button className="editBtn" onClick={edit}>↑ Edit ↑</button>
     </div>
   )
