a890231c4ab5fd18a43b55491628f24687d682af
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-08-13 11:52:41 +0200

    Fix const in PersonalCv

--- src/Components/PersonalCv.js
+++ src/Components/PersonalCv.js
@@ -1,7 +1,3 @@
 export function PersonalCv(props) {
-<<<<<<< HEAD
   const { firstName, lastName, profession, email, tel, address, website } = props.personalData;
-=======
-  const { firstName, lastName, profession, email, tel, address, website, display } = props
->>>>>>> personal-details-to-cv
 }
