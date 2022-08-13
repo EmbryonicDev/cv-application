f069644b423b25fca3f530146ff7d7374dc74220
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-08-13 12:17:26 +0200

    Create <ul> for contact details

--- src/Components/PersonalCv.js
+++ src/Components/PersonalCv.js
@@ -1,5 +1,10 @@
+import locationIcon from '../assets/location-icon.svg';
+import phoneIcon from '../assets/phone-icon.svg';
+import emailIcon from '../assets/email-icon.svg';
+import websiteIcon from '../assets/website-icon.svg';
+
 export function PersonalCv(props) {
-  const { firstName, lastName, profession, email, tel, address, website } = props.personalData;
+  const { firstName, lastName, profession, website, tel, address, email } = props.personalData;
 
   return (
     <div id="personalCv">
@@ -8,6 +13,24 @@ export function PersonalCv(props) {
         <h1 className="name">{lastName}</h1>
         <h3 className="professionText">{profession}</h3>
       </div>
+      <ul className="contactList">
+        <li className='contactData'>
+          <img src={locationIcon} alt="Location Icon" className="personalIcon" />
+          <h4 className='contactText'>{address}</h4>
+        </li>
+        <li className='contactData'>
+          <img src={phoneIcon} alt="Phone Icon" className="personalIcon" />
+          <h4 className='contactText'>{tel}</h4>
+        </li>
+        <li className='contactData'>
+          <img src={emailIcon} alt="Email Icon" className="emailIcon" />
+          <h4 className='contactText'>{email}</h4>
+        </li>
+        <li className='contactData'>
+          <img src={websiteIcon} alt="Website Icon" className="personalIcon" />
+          <h4 className='contactText'>{website}</h4>
+        </li>
+      </ul>
     </div>
   )
 }
