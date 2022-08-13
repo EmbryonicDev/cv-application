163d0978c3bbefbac4403c5f271c4136c5d128ad
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-08-13 12:04:52 +0200

    Create PersonalCv divs for name & professions

--- src/Components/PersonalCv.js
+++ src/Components/PersonalCv.js
@@ -1,3 +1,13 @@
 export function PersonalCv(props) {
   const { firstName, lastName, profession, email, tel, address, website } = props.personalData;
+
+  return (
+    <div id="personalCv">
+      <div id="nameDiv">
+        <h1 className="name">{firstName}</h1>
+        <h1 className="name">{lastName}</h1>
+        <h3 className="professionText">{profession}</h3>
+      </div>
+    </div>
+  )
 }
