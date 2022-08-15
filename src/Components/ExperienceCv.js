6943e8a378eb80950481e09e809de873260ae4e7
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-08-15 09:34:42 +0200

    Update variable names

--- src/Components/ExperienceCv.js
+++ src/Components/ExperienceCv.js
@@ -1,17 +1,17 @@
 export default function ExperienceCv(props) {
-  const { companyName, jobTitle, country, tasks, start, end } = props.experienceArr;
-  const [startYear, endYear] = [Date.getYear(start), Date.getYear(end)]
+  const { workName, workTitle, country, jobTasks, startDate, endDate } = props.experienceArr;
+  const [startYear, endYear] = [Date.getYear(startDate), Date.getYear(endDate)]
 
   return (
     <div className="experienceCv">
       <div className="companyDateCv">
-        <h3>{companyName}</h3>
+        <h3>{workName}</h3>
         <h4>{country}</h4>
         <h4>{startYear} - {endYear}</h4>
       </div>
       <div className="workTasksCv">
-        <h3>{jobTitle}</h3>
-        <p>{tasks}</p>
+        <h3>{workTitle}</h3>
+        <p>{jobTasks}</p>
       </div>
     </div>
   )
